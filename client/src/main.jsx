958aa7cffda8e8d68903a518a198b525a1b92d7b
Timothy Ryon <tim@timryon.com>
2024-09-04 12:12:29 -0400

    add theme for custom font

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import "./index.css";
 import routes from './AppRoutes';
+
+import CssBaseLine from "@mui/material/CssBaseLine";
+import { ThemeProvider } from "@mui/material/styles";
+
+import App from "./components/App";
+import theme from "./AppTheme";
+import "./index.css";
 
 const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router = { router } />);
+root.render(
+    <ThemeProvider theme={theme}>
+        <CssBaseLine />
+        <RouterProvider router = { router } />
+    </ThemeProvider>
+);
