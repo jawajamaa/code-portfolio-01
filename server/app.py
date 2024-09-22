eebe610c77ce74f1c06ebb3dd153c9739355acdd
Timothy Ryon <tim@timryon.com>
2024-09-21 21:24:38 -0400

    add config.py

--- server/app.py
+++ server/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+
+from config import app
 # from flask import Flask, make_response, jsonify
 # from flask_migrate import Migrate
 # from flask_restful import Resource
@@ -9,10 +11,10 @@ from flask import Flask
 
 # CORS(app)
 
-# if __name__ == '__main__':
-#     app.run(port=5555, debug=True)
-app = Flask(__name__)
 
 @app.route('/')
 def index():
     return '<h1>Welcome to the Jungle!!</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
