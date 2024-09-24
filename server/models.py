13c9357b53b57a9c82ccdfd9ea5cc3d3d8cfb7f0
Timothy Ryon <tim@timryon.com>
2024-09-23 22:04:26 -0400

    add 3 main model views

--- server/models.py
+++ server/models.py
@@ -3,14 +3,42 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-class Space(db.Model, SerializerMixin):
-    pass
+# Code Model view only
+class Code(db.Model, SerializerMixin):
+    __tablename__ = 'documents'
 
-class Place(db.Model, SerializerMixin):
-    pass
+    id = db.Column(db.Integer, primary_key = True)
+    item = db.Column(db.String)
+    path = db.Column(db.String)
 
-class Airport(db.Model, SerializerMixin):
-    pass
 
-class FromTheHip(db.Model, SerializerMixin):
-    pass
+class Image(db.Model, SerializerMixin):
+    __tablename__ = 'images'
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String)
+    location = db.Column(db.String)
+    year = db.Column(db.Integer)
+    gallery = db.Column(db.String)
+    horizontal = db.Column(db.Boolean)
+    path = db.Column(db.String)
+
+class PopQuiz(db.Model, SerializerMixin):
+    __tablename__ = "popquizquestions"
+
+    id = db.Column(db.Integer, primary_key = True)
+    question = db.Column(db.String)
+    answer = db.Column(db.String)
+
+# Photography Model views - not needed? 
+# class Airport(db.Model, SerializerMixin):
+#     pass
+
+# class FromTheHip(db.Model, SerializerMixin):
+#     pass
+
+# class Place(db.Model, SerializerMixin):
+#     pass
+
+# class Space(db.Model, SerializerMixin):
+#     pass
