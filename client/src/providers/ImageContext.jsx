9abcedd60a5352603a414bdc13c8f30d13d2cc76
Timothy Ryon <tim@timryon.com>
2024-10-03 22:04:19 -0400

    wip

--- client/src/providers/ImageContext.jsx
+++ client/src/providers/ImageContext.jsx
@@ -13,13 +13,23 @@ export function ImagesProvider({ children }) {
     console.log(imageUrl);
     console.log(images);
 
+    const randomIdx = () => {
+        return Math.floor(Math.random() * data.length)
+    }
+    // function randomIdx() {
+    //     return Math.floor(Math.random() * data.length)
+    // }
+
     useEffect(()=>{
         fetch(`${baseUrl + imageUrl}`)
             .then(r => r.json())
             .then(data => {
                 setImages(data);
-                const randomIdx = Math.floor(Math.random() * data.length);
+                // const randomIdx = Math.floor(Math.random() * data.length);
                 setRandomImage(data[randomIdx]);
+                if (randomImage.horizontal && randomImage.horizontal !== true){
+                    setRandomImage(data[randomIdx]);
+                }
             });
     }, []);
 
